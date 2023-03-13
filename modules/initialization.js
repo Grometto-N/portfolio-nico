4a915a15c32f1db90856dbf106c8089438bfb157
GROMETTO N <nicolas.grometto@laposte.net>
2023-03-13 17:44:31 +0100

    adding another project : morningnews typescipt

--- modules/initialization.js
+++ modules/initialization.js
@@ -48,20 +48,24 @@ function defProjectCapsule(){
     // définition des messages
     const messageTicket = "1er Hackathon de la formation : à partir d'une maquette sous forme de vidéo, nous avons codé de zéro un site de réservation de billets";
     const messageWordle = "Travailler les notions apprises en HTML et CSS pour coder de zéro une reproduction du jeu à partir de consignes et d'une vidéo";
-    const messageTweet = "2èe Hackathon de la formation : à partir d'une maquette sous forme de vidéo, nous avons codé de zéro une version simplifiée de tweeteur";
-
+    const messageTweet = "2ème Hackathon de la formation : à partir d'une maquette sous forme de vidéo, nous avons codé de zéro une version simplifiée de tweeteur";
+    const messageMorning = "Un projet de formation refait de zéro, mais en utilisant TypeScript pour le front-end";
 
     arrayToRetrun.push(defProject("TicketHack", messageTicket, ["HTML", "CSS","JavaScript", "DOM", "Node.js"], ["Node.js", "Express","MongoDB"], "TicketHack.png", 
                                  "https://ticket-hack-perso-frontend.vercel.app/", "https://github.com/Grometto-N/ticketHackPerso-frontend"
                                  , "https://github.com/Grometto-N/ticketHackPerso-backend"));
 
-    arrayToRetrun.push(defProject("Wordle", messageTicket, ["HTML", "CSS", "DOM", "JavaScript", "Node.js"], ["Node.js", "Express","MongoDB"], "WordleHTML.png", 
+    arrayToRetrun.push(defProject("Wordle", messageWordle, ["HTML", "CSS", "DOM", "JavaScript", "Node.js"], ["Node.js", "Express","MongoDB"], "WordleHTML.png", 
                                  "https://wordle-frontend-hazel.vercel.app/", "https://github.com/Grometto-N/wordle-frontend"
                                  , "https://github.com/Grometto-N/wordle-backend")); 
                                  
-    arrayToRetrun.push(defProject("HackaTweet", messageTicket, ["JavaScript", "React", "CSS", "Next.js","Node.js", "Redux"], ["Node.js", "Express", "MongoDB"], "HackaTweet.png", 
+    arrayToRetrun.push(defProject("HackaTweet", messageTweet, ["JavaScript", "React", "CSS", "Next.js","Node.js", "Redux"], ["Node.js", "Express", "MongoDB"], "HackaTweet.png", 
                                  "https://hackatweet-frontend-omega.vercel.app/", "https://github.com/Grometto-N/Hackatweet-Frontend"
-                                 , "https://github.com/Grometto-N/Hackatweet-Backend"));                              
+                                 , "https://github.com/Grometto-N/Hackatweet-Backend")); 
+    
+    arrayToRetrun.push(defProject("MorningNews", messageMorning, ["TypeScript", "React", "CSS", "Next.js","Node.js", "Redux"], ["Node.js", "Express", "MongoDB", "API externe"], "MorningNews.png", 
+                                 "https://morningnews-xi.vercel.app/", "https://github.com/Grometto-N/morningnews"
+                                 , "https://github.com/Grometto-N/morningnews-backend"));                             
 
     return arrayToRetrun;
 }
