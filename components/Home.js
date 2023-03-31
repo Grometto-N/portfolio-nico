f9f5b9f82b27325ae3193acd33b1ae5c9367aa7d
GROMETTO N <nicolas.grometto@laposte.net>
2023-03-31 12:02:13 +0200

    continue to factorizing code : change name variable and create a fonction for the length part

--- components/Home.js
+++ components/Home.js
@@ -21,7 +21,6 @@ function Home() {
 
   
 // définition et initialisation des états
-  const [yScroll, setYScroll] = useState(0); // pour obtenir la longueur scrollée
   const [barProgress, setBarProgress] = useState(0); // pour définir la longueur de la barre de progression
   const [componentsHeight, setComponentsHeight] = useState(initComponentHeight()) // pour obtenir la hauteur de chaque composant principal
   // const [startTextTranslation, setStartTextTranslation] = useState((initStartTranslation)) // pour savoir si l'animation du texte a déjà été lancé + un boolean de déclenchement
@@ -52,20 +51,16 @@ const isMobile = width <= 700;
 
   // initialisation d'une fonction gérant la longueur de la scrollBar pour la barre 
   useEffect(() => {
+    
     const handleScroll = event => {
-      // la hauteur scrollée sera assigné à l'état scrollY
-      setYScroll(window.scrollY);
-
       // définition des hauteurs scrollées déclenchant les animations
       const triggerLevelY = initTriggerLevel(); // les données initiales sont dans le module initialization
       let levelY = 0;
-      let sumBis = 0;
       for(let key in componentsHeight){
         if(Object.keys(triggerLevelY).some(elt=> elt === key)){
           triggerLevelY[key] = levelY ;
           levelY = levelY + componentsHeight[key];
         }
-        sumBis = sumBis + triggerLevelY[key];
       }
 
       // calcul de la hauteur totale des composants (sum) 
@@ -76,49 +71,6 @@ const isMobile = width <= 700;
       //     sum = sum + componentsHeight[key];
       //   }
       // }
-
-      // initialisation de textTranslationTempory qui permet de déclencher les animations quand on scolle
-      // const textTranslationTempory = startTextTranslation;
-      // initialisation de barPurcent pour gérer l'avancée de la barre de progression du header fixe
-    //   let barPurcent = 0;
-    //   const partSize = 100/getPlan().length; 
-    //   // cas de base : l'utilisateur a scrollé
-    //   if(window.scrollY>0){
-    //        barPurcent = (window.scrollY)/triggerLevelY.skills *partSize;
-    //   }
-
-    //   // l'utilisateur arrive aux compétences
-    //   if(window.scrollY > triggerLevelY.skills){
-    //       barPurcent = (window.scrollY-triggerLevelY.skills)/(triggerLevelY.projetsCapsule-triggerLevelY.skills) *partSize + partSize;
-    //     }
-
-    //   // l'utilisateur arrive aux projets de formations
-    //   if(window.scrollY > triggerLevelY.projetsCapsule){
-    //       barPurcent = (window.scrollY -triggerLevelY.projetsCapsule)/(triggerLevelY.autoformation -triggerLevelY.projetsCapsule) *partSize + partSize*2;
-    //   }
-
-    //   // l'utilisateur arrive aux projets de formations
-    //   if(window.scrollY > triggerLevelY.autoformation){
-    //     barPurcent = (window.scrollY -triggerLevelY.autoformation)/(triggerLevelY.projetsPerso-triggerLevelY.autoformation) *partSize+ partSize*3;
-    // }
-
-
-    //   // l'utilisateur arrive aux projets perso
-    //   if(window.scrollY > triggerLevelY.projetsPerso){
-    //       barPurcent = (window.scrollY -triggerLevelY.projetsPerso)/(triggerLevelY.contact-triggerLevelY.projetsPerso) *partSize+ partSize*4;
-    //   }
-
-
-
-    // // l'utilisateur arrive au niveau du contact 
-    // if(window.scrollY >triggerLevelY.contact && !isMobile){
-    //       barPurcent = window.scrollMaxY ? (window.scrollY -triggerLevelY.contact)/(window.scrollMaxY - triggerLevelY.contact) *partSize + partSize*4.96 :99.5;
-    // }
-
-    // if(window.scrollY > triggerLevelY.projetsPerso  && isMobile){
-    //   barPurcent =50000;
-    // }
-    
 
     // on met à jour les états pour la bar de progression et pour lancer les animations
       setBarProgress(progressBarLength(triggerLevelY,isMobile));
@@ -140,7 +92,7 @@ const isMobile = width <= 700;
  // variable d'affichage des différentes parties hors header fixe et contact
   const display = plan.map(elt =>{
   // return <Box key = {elt.name} getHeight={getHeight} title ={elt.title} name={elt.name} startAnimate={startTextTranslation[elt.name]}/>
-  return <Box key = {elt.name} getHeight={getHeight} title ={elt.title} name={elt.name} info={yScroll} />
+  return <Box key = {elt.name} getHeight={getHeight} title ={elt.title} name={elt.name} />
 })
 
 // const bar = isMobile ? `${barProgress}`: `${barProgress}`;
--- modules/progressBar.js
+++ modules/progressBar.js
@@ -20,50 +20,60 @@ function initTriggerLevel(){
     return objToReturn;
 }
 
+// fonction permettant d'avoir les valeurs correspondant de chaque parties sur la scrollBarY
+function getPartLevelY(){
+
+}
+
+// function calculer la longueur proportionnelle pour chaque partie
+function barLengthProportionPart(currentLevel, nextLevel){
+    return  (window.scrollY - currentLevel)/(nextLevel-currentLevel);
+} 
+
 // fonction permettant de calculer la longueur de la progress bar
-function progressBarLength(triggerLevelY, isMobile){
+function progressBarLength(partLevelY, isMobile){
     // calcule de la longueur de chaque partie (les div ayant la même taille on découpe selon le nombre de div)
     const partSize = 100/getPlan().length; 
 
     // l'utilisateur arrive au niveau du contact 
-    if(window.scrollY >triggerLevelY.contact && !isMobile){
+    if(window.scrollY >partLevelY.contact && !isMobile){
         // la méthode scrollMaxY n'existe pas dans tous les navigateurs
-        return window.scrollMaxY ? (window.scrollY -triggerLevelY.contact)/(window.scrollMaxY - triggerLevelY.contact) *partSize + partSize*4.96 :99.5;
+        return window.scrollMaxY ? (window.scrollY -partLevelY.contact)/(window.scrollMaxY - partLevelY.contact) *partSize + partSize*4.96 :99.5;
     }
 
     // pour mobile
-    if(window.scrollY > triggerLevelY.projetsPerso  && isMobile){
+    if(window.scrollY > partLevelY.projetsPerso  && isMobile){
         return 50000;
     }
 
     // l'utilisateur arrive aux projets perso
-    if(window.scrollY > triggerLevelY.projetsPerso){
-        return (window.scrollY -triggerLevelY.projetsPerso)/(triggerLevelY.contact-triggerLevelY.projetsPerso) *partSize+ partSize*4;
-    }
-
-    
-    // l'utilisateur arrive aux projets de formations
-    if(window.scrollY > triggerLevelY.autoformation){
-        return (window.scrollY -triggerLevelY.autoformation)/(triggerLevelY.projetsPerso-triggerLevelY.autoformation) *partSize+ partSize*3;
+    if(window.scrollY > partLevelY.projetsPerso){
+        // return (window.scrollY - levelY.projetsPerso)/(levelY.contact-levelY.projetsPerso) *partSize+ partSize*4;
+        return  barLengthProportionPart(partLevelY.projetsPerso,partLevelY.contact) *partSize+ partSize*4;
     }
 
     // l'utilisateur arrive aux projets de formations
-    if(window.scrollY > triggerLevelY.projetsCapsule){
-        return (window.scrollY -triggerLevelY.projetsCapsule)/(triggerLevelY.autoformation -triggerLevelY.projetsCapsule) *partSize + partSize*2;
+    if(window.scrollY > partLevelY.autoformation){
+        return barLengthProportionPart(partLevelY.autoformation,partLevelY.projetsPerso) *partSize+ partSize*3;
+    }
+
+    // l'utilisateur arrive aux projets de formations
+    if(window.scrollY > partLevelY.projetsCapsule){
+        return barLengthProportionPart(partLevelY.projetsCapsule,partLevelY.autoformation) *partSize + partSize*2;
     }
 
     // l'utilisateur arrive aux compétences
-    if(window.scrollY > triggerLevelY.skills){
-        return (window.scrollY-triggerLevelY.skills)/(triggerLevelY.projetsCapsule-triggerLevelY.skills) *partSize + partSize;
+    if(window.scrollY > partLevelY.skills){
+        return  barLengthProportionPart(partLevelY.skills,partLevelY.projetsCapsule) *partSize + partSize;
       }
 
     // cas de base : l'utilisateur a scrollé
     if(window.scrollY>0){
-        return (window.scrollY)/triggerLevelY.skills *partSize;
+        return  barLengthProportionPart(partLevelY.skills,0) *partSize;
     }
 
     //sécurité : renvoyer un nombre
     return 0;
 }
 
-module.exports = { initComponentHeight, initTriggerLevel, progressBarLength};
+module.exports = { initComponentHeight, initTriggerLevel, progressBarLength,getPartLevelY};
